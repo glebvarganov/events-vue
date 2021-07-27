3df26e40c1150345c0382a1029ef1bb00a84285b
Gleb Varganov <webgleb@gmail.com>
2021-07-28 03:00:01 +0500

    Start using Vuex

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,10 +1,25 @@
 <template>
-  <h1>Event create</h1>
+  <div>
+    <h1>Event create, {{ user.name }}</h1>
+    <p>This event was create by {{ user.id }}</p>
+    <p>There are {{ callLength }} categories:</p>
+    <ul>
+      <li v-for="cat in categories" :key="cat">{{ cat }}</li>
+    </ul>
+  </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 export default {
   name: 'EventCreate',
+  computed: {
+    callLength() {
+      return this.$store.getters.callLength
+    },
+    ...mapState(['user', 'categories']),
+  },
 }
 </script>
 
